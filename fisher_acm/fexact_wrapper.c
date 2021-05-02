d771d29cebea6a0fe1edc3a79c4d51fb30b93522
boussoffara <boussoffara@me.com>
2021-05-02 17:02:11 +0200

    github workflow

--- fisher_acm/fexact_wrapper.c
+++ fisher_acm/fexact_wrapper.c
@@ -8,14 +8,16 @@
 #include <math.h>
 #include <stdbool.h>
 
-#define PY3K (PY_VERSION_HEX >= 0x03000000)
+#if PY_MAJOR_VERSION >= 3
+#define PY3K
+#endif
 
 
 static PyObject *
 trace(PyObject *self, PyObject *args){
   PyArrayObject *array;
 //  double sum;
-  int i,j,k;
+  int i,j;
   if (!PyArg_ParseTuple(args, "O!",&PyArray_Type, &array)){
     PyErr_SetString(PyExc_ValueError,"Error while parsing the trajectory coordinates in get_spinangle_traj");
     return NULL;
@@ -30,7 +32,7 @@ trace(PyObject *self, PyObject *args){
     int ldtabl=array->dimensions[0];
     int ncol=array->dimensions[1];
 
-    int * table=malloc(nrow*ncol,sizeof(int));
+    int * table=malloc(nrow*ncol*sizeof(int));
 
     k=0;
     for (j = 0; j < ncol; j++){
@@ -66,6 +68,9 @@ static PyMethodDef fexact_methods[] = {
 	{NULL,		NULL}		/* sentinel */
 };
 
+
+#ifdef PY3K
+
 static struct PyModuleDef fexactmodule = {
     PyModuleDef_HEAD_INIT,
     "fexact",   /* name of module */
@@ -74,8 +79,15 @@ static struct PyModuleDef fexactmodule = {
     fexact_methods
 };
 
+
 PyMODINIT_FUNC PyInit_fexact(void)
 {   import_array();
     return PyModule_Create(&fexactmodule);
 }
 
+#else
+
+PyMODINIT_FUNC initfexact() {
+    Py_InitModule3("fexact", fexact_methods, "mod doc");
+}
+#endif
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup, Extension
 import numpy
 setup(name             = "fexact",
-      version          = "1.0",
-      description      = "Example code for blog post.",
-      author           = "J. David Lee",
-      author_email     = "contact@crumpington.com",
-      maintainer       = "contact@crumpington.com",
-      url              = "https://www.crumpington.com",
+      version          = "0.0.1",
+      description      = "Exact Fisher test for nxm contingency matrix based on fortran acm 643 and R",
+      author           = "A. Boussoffara",
+      author_email     = "boussoffara@me.com",
+      maintainer       = "boussoffara@me.com",
+      url              = "https://www.github.com/boussoffara/fisher-acm",
       ext_modules      = [
           Extension(
               'fexact', ['fisher_acm/fexact_wrapper.c','fisher_acm/fexact.c','fisher_acm/helpers.c'],include_dirs=[numpy.get_include()],
