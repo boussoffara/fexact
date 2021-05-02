83d735c40381770c1e3cf1ff445953b169a20a37
boussoffara <boussoffara@me.com>
2021-05-02 18:28:56 +0200

    github workflow

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup, Extension
 import numpy
+
+
+current_folder = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(current_folder, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+
 setup(name             = "fexact",
       version          = "0.0.3",
       description      = "Exact Fisher test for nxm contingency matrix based on fortran acm 643 and R",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      license='GPLv3',
       author           = "A. Boussoffara",
       author_email     = "boussoffara@me.com",
       maintainer       = "boussoffara@me.com",
