4c7bf3758ec076549589066b31b8914cadd36be5
boussoffara <boussoffara@me.com>
2021-05-02 20:11:26 +0200

    github workflow

--- tests/test_fexact.py
+++ tests/test_fexact.py
@@ -1,4 +1,9 @@
-from fexact import fexact
 import numpy as np
-k=np.array([[5,1,5],[1,1,5]], dtype=np.intc)
-print(fexact(k))
+print(np.__version__)
+from fexact import fexact
+
+def test_fexact_small():
+
+    k=np.array([[5,1,5],[1,1,5]], dtype=np.intc)
+    res=fexact(k)
+    assert np.abs(0.4004524886877815 -res)<10e-5
